90668bd562895546ff57aaf18607322de34da431
jonnew <jpnewman@mit.edu>
2015-02-21 18:05:40 -0500

    Initial commit

---
+++ src/CameraConfig.h
@@ -0,0 +1,23 @@
+/* Camera configuration for rat tracker use */
+#ifndef CameraConfig_H
+#define CameraConfig_H
+
+// TODO: #defines for setting camera trigger modes
+
+// Forward declaration of CameraInfo class
+namespace FlyCapture2 {
+    class CameraInfo;
+}
+
+class CameraConfig {
+
+    public:
+        CameraConfig(void);
+        void print_camera_info(FlyCapture2::CameraInfo *p_cam_info);
+        int set_gpio_trigger(void); //TODO: GPIO type
+    
+    private:
+
+};
+
+#endif
---
+++ src/RatVision.cpp
@@ -0,0 +1,16 @@
+/* Implementation */
+
+#include "CameraConfig.h"
+#include "FlyCapture2.h"
+
+int main(int argc, char *argv[]) {
+   
+    //FlyCapture2::Camera cam();
+
+    // Create a CameraConfig object
+    CameraConfig cc();
+
+    // Exit
+    return 0;
+
+}   
