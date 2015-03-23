a7212efc0837e7fde60cad0cf032b5520b014682
jonnew <jpnewman@mit.edu>
2015-03-23 09:51:23 -0400

    Added point-grey datasheets. Started to flesh out CameraConfig class.

--- src/CameraConfig.cpp
+++ src/CameraConfig.cpp
@@ -5,27 +5,31 @@
 
 CameraConfig::CameraConfig(void) {
 
+	cam_info = 
 }
 
-void CameraConfig::print_camera_info(FlyCapture2::CameraInfo *p_cam_info) {
-    std::cout << std::endl;
-	std::cout << "*** GENERAL CAMERA INFORMATION ***" << std::endl;
-	std::cout << "Serial number -" << p_cam_info->serialNumber << std::endl;
-    std::cout << "Camera model - " << p_cam_info->modelName << std::endl;
-    std::cout << "Camera vendor - " << p_cam_info->vendorName << std::endl;
-    std::cout << "Sensor - " << p_cam_info->sensorInfo << std::endl;
-    std::cout << "Resolution - " << p_cam_info->sensorResolution << std::endl;
-    std::cout << "Firmware version - " << p_cam_info->firmwareVersion << std::endl;
-    std::cout << "Firmware build time - " << p_cam_info->firmwareBuildTime << std::endl << std::endl;
+void CameraConfig::print_camera_info(void) {
+	if (cam_info != null) { 
 
-    // Here we need to extend this method to print things about
-    // triggering and other hardware settings specific to the CameraConfig
-    // Class
-	
+		std::cout << std::endl;
+		std::cout << "*** GENERAL CAMERA INFORMATION ***" << std::endl;
+		std::cout << "Serial number -" << cam_info.serialNumber << std::endl;
+		std::cout << "Camera model - " << cam_info.modelName << std::endl;
+		std::cout << "Camera vendor - " << cam_info.vendorName << std::endl;
+		std::cout << "Sensor - " << cam_info.sensorInfo << std::endl;
+		std::cout << "Resolution - " << cam_info.sensorResolution << std::endl;
+		std::cout << "Firmware version - " << cam_info.firmwareVersion << std::endl;
+		std::cout << "Firmware build time - " << cam_info.firmwareBuildTime << std::endl << std::endl;
+
+		// Here we need to extend this method to print things about
+		// triggering and other hardware settings specific to the CameraConfig
+		// Class
+		// }
+
 }
 
 int CameraConfig::set_gpio_trigger(void) {
 
-    return 0;
+	return 0;
 }
 
--- src/CameraConfig.h
+++ src/CameraConfig.h
@@ -10,16 +10,30 @@ namespace FlyCapture2 {
     class CameraInfo;
 }
 
+
+typedef enum {
+	GPIO0,
+	GPIO1,
+	GPIO2,
+	GPIO3,
+	GPIO4,
+} GPIO;
+
+
 class CameraConfig {
 
     public:
         CameraConfig(void);
-        void print_camera_info(FlyCapture2::CameraInfo *p_cam_info);
-        int set_gpio_trigger(void); //TODO: GPIO type
+		void get_camera_info(void);
+        void print_camera_info(void);
+        int set_gpio_trigger(GPIO gpio); //TODO: GPIO type
    		int set_ip(string ip); 
+   		int verify_ext_trigger_support(string ip); 
 		
     private:
-
+		FlyCapture2::CameraInfo &cam_info;
+		FlyCapture2::TriggerMode &trig_mode;
+		void print_error(void);
 };
 
 #endif
