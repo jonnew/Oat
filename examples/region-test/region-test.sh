a4c1c34255907afc33a3b2ef397b4db6244b8bdf
jonnew <jpnewman@mit.edu>
2015-12-21 11:32:05 -0500

    Bunch of minor thoughts and changes
    - Thinking about multiposition type
    - Config.toml examples clean up
    - More decorator improvments

--- examples/region-test/region-test.sh
+++ examples/region-test/region-test.sh
@@ -4,13 +4,15 @@ case "$1" in
 
 	run)
 		oat view final                                        & 
-		oat decorate raw final -p pr1 pr2 pr3 -SsR            & 
+		oat decorate raw final -p pr1 pr2 pr3 pcr -SsR        & 
+		oat posifilt region pc pcr -c config.toml region      & 
 		oat posifilt region p3 pr3 -c config.toml region      & 
 		oat posifilt region p2 pr2 -c config.toml region      & 
 		oat posifilt region p1 pr1 -c config.toml region      & 
-        oat posigen rand2D p3 -c config.toml p3               & 
-        oat posigen rand2D p2 -c config.toml p2               & 
-        oat posigen rand2D p1 -c config.toml p1               & 
+        oat posicom mean p1 p2 pc -c config.toml combiner     &
+        oat posigen rand2D p3 -c config.toml pgen             & 
+        oat posigen rand2D p2 -c config.toml pgen             & 
+        oat posigen rand2D p1 -c config.toml pgen             & 
 
 		sleep 1
 		oat frameserve file raw -f ~/Desktop/rat.avi -c config.toml video
@@ -22,7 +24,7 @@ case "$1" in
 		;;
 
 	*)
-		echo $"Usage: $0 {run|clean|kill}"
+		echo $"Usage: $0 {run|clean}"
 		exit 1
 
 esac
