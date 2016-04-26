36e997c52f4e0f6376c97a416989bb38da161540
jonnew <jpnewman@mit.edu>
2016-04-26 17:10:46 -0400

    Got rid of a bunch of mistakenly added files.

--- test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jon/public/Oat/test
-# Build directory: /home/jon/public/Oat/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(shmemdf)
