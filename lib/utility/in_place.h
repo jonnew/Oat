e132d3ed0802776d4c0a15a79a93150ae9648e7f
jonnew <jpnewman@mit.edu>
2016-08-25 21:22:12 -0400

    Added missing in_place.h file.

---
+++ lib/utility/in_place.h
@@ -0,0 +1,12 @@
+// C++11 in_place constructor argument substitute
+#ifndef STD_INPLACE_H
+#define STD_INPLACE_H
+
+namespace std {
+
+template <typename T> 
+struct in_place {};
+
+}
+
+#endif /* STD_INPLACE_H */
